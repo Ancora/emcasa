18e9270891023a4a78ec7618027b1080ee59897e
Ancora <anderson.rocha@ancora-ti.com.br>
2020-05-08 18:23:10 -0300

    Custom layout e create_stores_table.

--- database/migrations/2020_05_08_204246_create_stores_table.php
+++ database/migrations/2020_05_08_204246_create_stores_table.php
@@ -15,7 +15,29 @@ class CreateStoresTable extends Migration
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('user_id');                      // id do usuário logado
+
+            $table->string('name')->unique();                           // razão social ou nome
+            $table->string('register')->unique();                       // cnpj ou cpf
+            $table->string('company_name')->unique();                   // nome fantasia
+            $table->string('description')->nullable();                  // descrição das atividades
+            $table->string('prefix')->unique();                         // prefixo dos códigos dos produtos
+            $table->string('slug');                                     // razao-social-ou-nome para url curta
+            $table->string('public_place');                             // logradouro
+            $table->integer('number');                                  // número do logradouro
+            $table->string('complement')->nullable();                   // complemento do endereço
+            $table->string('district');                                 // bairro
+            $table->string('city');                                     // cidade
+            $table->string('country');                                  // uf
+            $table->string('postal_code');                              // cep
+            $table->string('phone');                                    // telefone fixo
+            $table->string('mobile_phone')->nullable();                 // telefone celular
+            $table->string('email');                                    // e-mail
+            $table->decimal('delivery_fee', 5, 2)->nullable();          // taxa de entrega padrão da loja
+
+            $table->timestamps();                                       // data e hora
+
+            $table->foreign('user_id')->references('id')->on('users');  // chave estrangeira (stores_user_id_foreign)
         });
     }
 
